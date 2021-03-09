ce7fb5fef01371dd5505c79dc2fa5bed146e385d
Christopher Kyle <ckyle6300@outlook.com>
2021-03-09 16:28:01 -0600

    updated schema

--- express-project-starter/routes/index.js
+++ express-project-starter/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const { Story, Like, User } = require('../db/models');
+const { asyncHandler } = require('../utils');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const stories = await Story.findAll({
+    include: [User, Like]
+  })
+  res.render('index', { title: 'Welcome', stories });
+}));
 
 module.exports = router;
