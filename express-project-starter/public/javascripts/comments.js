773cb59b3cef46148a82e08ccc1223c64a4fa8a9
KevinKaras <kevinkaras2@gmail.com>
2021-03-10 13:20:14 -0600

    lunch break finished posting comments without any errors

--- express-project-starter/public/javascripts/comments.js
+++ express-project-starter/public/javascripts/comments.js
@@ -5,11 +5,19 @@ window.addEventListener('DOMContentLoaded', async(event)=>{
 
         const textBox = document.querySelector(".newComment__text")
         const storyId = textBox.getAttribute("storyid") 
-        const body = textBox.innerText
+        const commentBody = textBox.innerText
+        // console.log(commentBody)
         // toDo - how to verify that the person sending this comment is logged in?
 
-        const newComment = await fetch()
+        const body = { commentBody, storyId }
 
+        const newComment = await fetch("/comments", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {"Content-Type": "application/json"}
+        })
+        // console.log(newComment)
+        textBox.innerText = ""
     })
 
 
