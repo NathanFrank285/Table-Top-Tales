a52e30b7c0092ee4e7fef2c62111467cc6a09697
NathanFrank285 <nathansfrank@yahoo.com>
2021-03-10 18:14:12 -0800

    starting the delete router

--- express-project-starter/routes/stories.js
+++ express-project-starter/routes/stories.js
@@ -62,11 +62,16 @@ storiesRouter.get('/:id', asyncHandler(async (req, res, next)=> {
     include: { model: User },
     order: [["id", "ASC"]]
   });
-  res.locals.authorId = story.author.id
+  res.locals.userId = req.session.auth.userId;
 
 
   res.render("story-view", { story, comments });
   // res.json(res.locals)
+}))
+
+storiesRouter.delete('/:id/delete', asyncHandler(async (req, res) => {
+  //todo 
+
 }))
 
 module.exports = storiesRouter
